8c46967916f750c3748ca9661c5173ef98204a0a
Jack Cullen <jpcullen20@gmail.com>
2021-04-29 14:14:51 -0600

    Add test for purchasing a ticket

--- test/raffle-test.js
+++ test/raffle-test.js
@@ -1,3 +1,4 @@
+const BN = require("bn.js");
 const { expect } = require("chai");
 
 describe("Raffle", function() {
@@ -10,5 +11,16 @@ describe("Raffle", function() {
     expect(await raffle.owner()).to.equal(accounts[0].address);
     expect(await raffle.beneficiary()).to.equal(accounts[1].address);
     expect(await raffle.ticketCost()).to.equal(100);
+  });
+
+  it("Allows an account to purchase a ticket", async function() {
+    const accounts = await ethers.getSigners();
+    const Raffle = await ethers.getContractFactory("Raffle");
+    const raffle = await Raffle.deploy(100, accounts[1].address);
+    await raffle.deployed();
+
+    expect(await raffle.ticketCount(accounts[2].address)).to.equal(0);
+    await raffle.purchaseTicket({ from: accounts[2], value: new BN('100') } )
+    expect(await raffle.ticketCount(accounts[2].address)).to.equal(1);
   });
 });
