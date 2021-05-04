f685cb34a0fa4f9e9de5275fc3b7375917e185ce
Jack Cullen <jpcullen20@gmail.com>
2021-05-04 14:08:47 -0600

    Add factory contract

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -7,20 +7,35 @@ import "hardhat/console.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 // import "@chainlink/contracts/src/v0.6/VRFConsumerBase.sol";
 
+contract RaffleFactory {
+    address[] public deployedRaffles;
+
+    function createRaffle(uint _ticketPrice, address payable _beneficiary) public {
+        Raffle newRaffle = new Raffle(_ticketPrice, _beneficiary, msg.sender);
+        deployedRaffles.push(address(newRaffle));
+    }
+
+    function getDeployedRaffles() public view returns(address[] memory) {
+        return deployedRaffles;
+    }
+}
+
 contract Raffle is Ownable {
     uint256 public ticketPrice;
     address payable public beneficiary;
     mapping(address => uint256) public ticketCount;
     address[] public allTicketHolders;
 
-    constructor(uint256 _ticketPrice, address payable _beneficiary) {
+    constructor(uint256 _ticketPrice, address payable _beneficiary, address _owner) {
         console.log(
-            "Deploying a raffle with ticketPrice: '%s' and beneficiary: '%s'",
+            "Deploying a raffle with ticketPrice: '%s', beneficiary: '%s', and owner: '%s'",
             _ticketPrice,
-            _beneficiary
+            _beneficiary,
+            _owner
         );
         ticketPrice = _ticketPrice;
         beneficiary = _beneficiary;
+        transferOwnership(_owner);
     }
 
     function purchaseTicket() public payable {
