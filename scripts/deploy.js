e699e5183d33f5bd82a97eae1e59ff5bdacb9973
Jack Cullen <jpcullen20@gmail.com>
2021-05-05 10:56:00 -0600

    Update deploy script to deploy the raffle factory

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -14,12 +14,11 @@ async function main() {
   // await hre.run('compile');
 
   // We get the contract to deploy
-  const Greeter = await hre.ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  const RaffleFactory = await hre.ethers.getContractFactory("RaffleFactory");
+  const raffleFactory = await RaffleFactory.deploy();
+  await raffleFactory.deployed();
 
-  await greeter.deployed();
-
-  console.log("Greeter deployed to:", greeter.address);
+  console.log("RaffleFactory deployed to:", raffleFactory.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
