2bef8f4d0106dec1a0817e3db03bc19c3db3f4e4
Jack Cullen <jpcullen20@gmail.com>
2021-05-05 11:23:04 -0600

    Render address to all raffles

--- src/App.js
+++ src/App.js
@@ -5,11 +5,12 @@ import RaffleFactory from "./artifacts/contracts/Raffle.sol/RaffleFactory.json";
 import Raffle from "./artifacts/contracts/Raffle.sol/Raffle.json";
 
 const raffleFactoryAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"; // local
-// const greeterAddress = "0xeee7874BaF2BFEB1df7E09D55A56594A50ACFae2"; // ropsten
+// const raffleFactoryAddress = "0xeee7874BaF2BFEB1df7E09D55A56594A50ACFae2"; // ropsten
 
 function App() {
   const [ticketPrice, setTicketPriceValue] = useState(0.01);
   const [beneficiary, setBeneficiaryValue] = useState("");
+  const [raffles, setRafflesValue] = useState([]);
 
   async function requestAccount() {
     await window.ethereum.request({ method: "eth_requestAccounts" });
@@ -19,42 +20,16 @@ function App() {
     await requestAccount();
     const provider = new ethers.providers.Web3Provider(window.ethereum);
     const signer = provider.getSigner();
-    const address  = await signer.getAddress()
+    const address = await signer.getAddress();
     setBeneficiaryValue(address);
+    const factory = new ethers.Contract(
+      raffleFactoryAddress,
+      RaffleFactory.abi,
+      signer
+    );
+    const allRaffles = await factory.getDeployedRaffles();
+    setRafflesValue([...allRaffles]);
   });
-
-  // async function fetchGreeting() {
-  //   if (typeof window.ethereum !== "undefined") {
-  //     const provider = new ethers.providers.Web3Provider(window.ethereum);
-  //     console.log({ provider });
-  //     const contract = new ethers.Contract(
-  //       greeterAddress,
-  //       Greeter.abi,
-  //       provider
-  //     );
-  //     try {
-  //       const data = await contract.greet();
-  //       console.log("data: ", data);
-  //     } catch (err) {
-  //       console.log("Error: ", err);
-  //     }
-  //   }
-  // }
-
-  // async function setGreeting() {
-  //   if (!greeting) return;
-  //   if (typeof window.ethereum !== "undefined") {
-  //     await requestAccount();
-  //     const provider = new ethers.providers.Web3Provider(window.ethereum);
-  //     console.log({ provider });
-  //     const signer = provider.getSigner();
-  //     const contract = new ethers.Contract(greeterAddress, Greeter.abi, signer);
-  //     const transaction = await contract.setGreeting(greeting);
-  //     setGreetingValue('')
-  //     await transaction.wait();
-  //     fetchGreeting();
-  //   }
-  // }
 
   async function deployRaffle() {
     if (!ticketPrice || !beneficiary) return;
@@ -68,15 +43,20 @@ function App() {
         signer
       );
       const formattedPrice = ethers.utils.parseEther(ticketPrice.toString());
-      await factory.createRaffle(formattedPrice, beneficiary);
       try {
-        const raffles = await factory.getDeployedRaffles();
-        console.log("raffles: ", raffles);
+        await factory.createRaffle(formattedPrice, beneficiary);
       } catch (err) {
         console.log("Error: ", err);
       }
     }
   }
+
+   function renderRaffles() {
+    return (raffles.map((raffle) => {
+      return <p>{raffle}</p>
+    }))
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -91,6 +71,7 @@ function App() {
           placeholder="Beneficiary"
           value={beneficiary}
         />
+        <section>{renderRaffles()}</section>
       </header>
     </div>
   );
