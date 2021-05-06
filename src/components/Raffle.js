ba938e87553b4585645588f7a7ffd0b33bf695b1
Jack Cullen <jpcullen20@gmail.com>
2021-05-06 12:35:58 -0600

    Add button to purchase ticket and display tickets owned

--- src/components/Raffle.js
+++ src/components/Raffle.js
@@ -3,9 +3,12 @@ import { ethers } from "ethers";
 import RaffleContract from "../artifacts/contracts/Raffle.sol/Raffle.json";
 
 const Raffle = ({ raffleAddress }) => {
+  // pass some of these down as props
   const [ticketPrice, setTicketPriceValue] = useState('Loading...');
   const [beneficiary, setBeneficiaryValue] = useState('');
   const [balance, setBalanceValue] = useState('');
+  const [userAddress, setUserAddressValue] = useState('');
+  const [userTicketCount, setUserTicketCountValue] = useState('');
 
   async function requestAccount() {
       await window.ethereum.request({ method: "eth_requestAccounts" });
@@ -22,17 +25,43 @@ const Raffle = ({ raffleAddress }) => {
     );
     const raffleTicketPrice = await deployedRaffle.ticketPrice();
     const raffleBeneficiary = await deployedRaffle.beneficiary();
+    const address = await signer.getAddress();
+    const ticketCount = await deployedRaffle.ticketCount(address);
     const formattedPrice = ethers.utils.formatEther(raffleTicketPrice.toString());
     const contractBalance = await provider.getBalance(raffleAddress)
+    setUserAddressValue(address)
     setTicketPriceValue(formattedPrice);
+    setUserTicketCountValue(ticketCount.toString());
     setBeneficiaryValue(raffleBeneficiary);
     setBalanceValue(ethers.utils.formatEther(contractBalance.toString()));
   });
 
+  async function purchaseTicket() {
+    await requestAccount();
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner();
+    const deployedRaffle = new ethers.Contract(
+      raffleAddress,
+      RaffleContract.abi,
+      signer
+    );
+    const ethTicketPrice = ethers.utils.parseEther(ticketPrice.toString());
+    try {
+      await deployedRaffle.purchaseTicket({
+        from: userAddress,
+        value: ethTicketPrice,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
   
   return (
     <div>
-      Ticket Price: {ticketPrice} ETH, Balance: {balance} ETH, Beneficiary: {beneficiary.slice(0, 10)}...
+      <button onClick={purchaseTicket}>Purchase Ticket</button>
+      Ticket Price: {ticketPrice} ETH, Balance: {balance} ETH, Beneficiary:{" "}
+      {beneficiary.slice(0, 5)}...
+      TicketsOwned: {userTicketCount}
     </div>
   );
 }
