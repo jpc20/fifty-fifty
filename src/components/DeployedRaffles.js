02c1842f007d7e9728e9cc8b125aa48feaf29d90
Jack Cullen <jpcullen20@gmail.com>
2021-05-06 11:08:13 -0600

    WIP -- displaying each deployed raffle

--- src/components/DeployedRaffles.js
+++ src/components/DeployedRaffles.js
@@ -1,11 +1,12 @@
 import Raffle from "./Raffle";
 
 const DeployedRaffles = ({ raffles }) => {
+  const raffleComponents = raffles.map((raffleAddress) => {
+    return <Raffle raffleAddress={raffleAddress} key={raffleAddress} />;
+  });
   return (
     <div>
-      {raffles.map((raffleAddress) => {
-        return <Raffle raffleAddress={raffleAddress} key={raffleAddress} />;
-      })}
+      {raffleComponents}
     </div>
   );
 };
--- src/components/Raffle.js
+++ src/components/Raffle.js
@@ -3,8 +3,10 @@ import { ethers } from "ethers";
 import RaffleContract from "../artifacts/contracts/Raffle.sol/Raffle.json";
 
 const Raffle = ({ raffleAddress }) => {
-  const [raffle, setRaffleValue] = useState({});
-  
+  const [ticketPrice, setTicketPriceValue] = useState('Loading...');
+  const [beneficiary, setBeneficiaryValue] = useState('');
+  const [balance, setBalanceValue] = useState('');
+
   async function requestAccount() {
       await window.ethereum.request({ method: "eth_requestAccounts" });
   }
@@ -14,16 +16,23 @@ const Raffle = ({ raffleAddress }) => {
     const provider = new ethers.providers.Web3Provider(window.ethereum);
     const signer = provider.getSigner();
     const deployedRaffle = new ethers.Contract(
-        raffleAddress,
+      raffleAddress,
       RaffleContract.abi,
       signer
     );
-    setRaffleValue(deployedRaffle);
+    const raffleTicketPrice = await deployedRaffle.ticketPrice();
+    const raffleBeneficiary = await deployedRaffle.beneficiary();
+    const formattedPrice = ethers.utils.formatEther(raffleTicketPrice.toString());
+    const contractBalance = await provider.getBalance(raffleAddress)
+    setTicketPriceValue(formattedPrice);
+    setBeneficiaryValue(raffleBeneficiary);
+    setBalanceValue(ethers.utils.formatEther(contractBalance.toString()));
   });
+
   
   return (
     <div>
-        Test
+      Ticket Price: {ticketPrice} ETH, Balance: {balance} ETH, Beneficiary: {beneficiary.slice(0, 10)}...
     </div>
   );
 }
