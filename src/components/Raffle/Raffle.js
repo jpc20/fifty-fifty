7e2e50619487bad066fdc852bbb76895c1edc3e1
Jack Cullen <jpcullen20@gmail.com>
2021-05-18 15:55:44 -0600

    Move logic up to parent component

--- src/components/Raffle/Raffle.js
+++ src/components/Raffle/Raffle.js
@@ -1,8 +1,8 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { ethers } from "ethers";
 import RaffleContract from "../../artifacts/contracts/Raffle.sol/Raffle.json";
 import { makeStyles } from "@material-ui/core/styles";
-import { Button, Grid, Typography, Paper } from "@material-ui/core";
+import { Grid, Typography, Paper } from "@material-ui/core";
 import LoadingButton from "../LoadingButton";
 
 const useStyles = makeStyles((theme) => ({
@@ -18,45 +18,21 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const Raffle = ({ raffleAddress, getSignerAndProvider, raffleFilter }) => {
-  const [beneficiary, setBeneficiaryValue] = useState("");
-  const [balance, setBalanceValue] = useState(0);
-  const [userTicketCount, setUserTicketCountValue] = useState(0);
-  const [totalTicketCount, setTotalTicketCountValue] = useState(0);
-  const [raffleTicketPrice, setRaffleTickerPriceValue] = useState(0);
-  const [isOwner, setIsOwnerValue] = useState(false);
-  const [open, setOpenValue] = useState(null);
+const Raffle = ({
+  getSignerAndProvider,
+  raffleTicketPrice,
+  beneficiary,
+  userTicketCount,
+  totalTicketCount,
+  balance,
+  isOwner,
+  open,
+  raffleAddress,
+  raffleFilter,
+}) => {
   const [purchaseLoading, setPurchaseLoadingValue] = useState(false);
   const [distributeLoading, setDistributeLoadingValue] = useState(false);
   const classes = useStyles();
-
-  useEffect(() => {
-    const getRaffle = async () => {
-      const [provider, signer, address] = await getSignerAndProvider();
-      const deployedRaffle = new ethers.Contract(
-        raffleAddress,
-        RaffleContract.abi,
-        signer
-      );
-      const raffleTicketPrice = await deployedRaffle.ticketPrice();
-      const raffleBeneficiary = await deployedRaffle.beneficiary();
-      const userTickets = await deployedRaffle.ticketCount(address);
-      const allTicketHolders = await deployedRaffle.getTicketHolders();
-      const contractBalance = await provider.getBalance(raffleAddress);
-      const checkOwner = await deployedRaffle.owner();
-      const openStatus = await deployedRaffle.open();
-      setRaffleTickerPriceValue(
-        ethers.utils.formatEther(raffleTicketPrice.toString())
-      );
-      setUserTicketCountValue(userTickets.toNumber());
-      setTotalTicketCountValue(allTicketHolders.length);
-      setBeneficiaryValue(raffleBeneficiary);
-      setBalanceValue(ethers.utils.formatEther(contractBalance.toString()));
-      setOpenValue(openStatus);
-      setIsOwnerValue(checkOwner === address);
-    };
-    getRaffle();
-  }, []);
 
   async function purchaseTicket() {
     setPurchaseLoadingValue(true);
@@ -75,9 +51,9 @@ const Raffle = ({ raffleAddress, getSignerAndProvider, raffleFilter }) => {
         value: ethTicketPrice,
       });
       provider.once(purchaseTx.hash, (transaction) => {
-        setUserTicketCountValue((userTicketCount) => userTicketCount + 1);
+        // setUserTicketCountValue((userTicketCount) => userTicketCount + 1);
         setPurchaseLoadingValue(false);
-        setBalanceValue(parseFloat(balance) + parseFloat(raffleTicketPrice));
+        // setBalanceValue(parseFloat(balance) + parseFloat(raffleTicketPrice));
       });
     } catch (error) {
       console.log(error);
