01413ff68d7db4d212f46206029de12b748cd089
Jack Cullen <jpcullen20@gmail.com>
2021-07-16 09:49:56 -0600

    Update test to account for bug where admin cannot close a raffle with outstanding tickets and a different owner

--- test/contracts/raffleFactory-test.js
+++ test/contracts/raffleFactory-test.js
@@ -32,15 +32,16 @@ describe("RaffleFactory", function () {
     const ticketPrice = ethers.utils.parseEther(".1");
     const description = "test description";
     const symbol = "TEST_TKT";
-    await factory.createRaffle(
-      description,
-      symbol,
-      ticketPrice,
-      accounts[1].address
-    );
+    await factory
+      .connect(accounts[1])
+      .createRaffle(description, symbol, ticketPrice, accounts[1].address);
     const raffles = await factory.getDeployedRaffles();
-    const raffle = new ethers.Contract(raffles[0], Raffle.abi, accounts[0]);
+    const raffle = new ethers.Contract(raffles[0], Raffle.abi, accounts[2]);
     expect(await factory.admin()).to.equal(accounts[0].address);
+    expect(await raffle.owner()).to.equal(accounts[1].address);
+    await raffle
+      .connect(accounts[3])
+      .purchaseTicket({ from: accounts[3].address, value: ticketPrice });
     expect(await raffle.open()).to.equal(true);
     await factory.connect(accounts[0]).closeRaffle(raffle.address);
     expect(await raffle.open()).to.equal(false);
